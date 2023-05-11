787af2ade528c840a9ae051841960934851db9b3
wonseok <nsk7640@naver.com>
2023-05-11 11:28:03 +0900

    user-service eureka 서버에 ip로 나타나게 변경

--- user-service/src/main/kotlin/io/seok/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/io/seok/userservice/UserServiceApplication.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient // 디스커버리 클라이언트 어노테이션을 안써도 dependency만 설정 되어있으면 자동등록됨 ( 명시적으로 보여주기 위해 해당 어노테이션을 설정함 )
 class UserServiceApplication {
 
     @Bean
