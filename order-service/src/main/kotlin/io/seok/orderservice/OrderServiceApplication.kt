f1fe3f966f60d277a66ccad4cd717753a2c0da89
SeokSeokNa <nsk7640@naver.com>
2023-07-16 22:14:17 +0900

    prometheus 적용

--- order-service/src/main/kotlin/io/seok/orderservice/OrderServiceApplication.kt
+++ order-service/src/main/kotlin/io/seok/orderservice/OrderServiceApplication.kt
@@ -1,11 +1,20 @@
 package io.seok.orderservice
 
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class OrderServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<OrderServiceApplication>(*args)
+}
+
+
+@Bean
+fun httpTraceRepository(): HttpExchangeRepository {
+    return InMemoryHttpExchangeRepository()
 }
