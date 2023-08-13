6175630a773f19596314a77796532ed7071162d6
SeokSeokNa <nsk7640@naver.com>
2023-08-13 11:57:33 +0900

    catalog service kafka config 수정

--- catalog-service/src/main/kotlin/io/seok/catalogservice/messagequeue/KafkaConsumerConfig.kt
+++ catalog-service/src/main/kotlin/io/seok/catalogservice/messagequeue/KafkaConsumerConfig.kt
@@ -18,7 +18,7 @@ class KafkaConsumerConfig {
     @Bean
     fun consumerFactory(): ConsumerFactory<String, String> {
         val properties = hashMapOf<String, Any>(
-            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "127.0.0.1:9092",
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "broker:9092", // brocker docker container의 ip 주소
             ConsumerConfig.GROUP_ID_CONFIG to "consumerGroupId", //group id는 topic에 쌓여있는 메세지를 가져갈 수 있는 그룹을 말한다.(특정 consumer 들만 데이터를 수신할 수 있도록 하기위해 그룹 Id를 만든다)
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
